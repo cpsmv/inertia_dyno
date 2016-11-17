d6fc60d84e74555222be778bf6093d27b5dd7c92
Alex Pink <alexdpink@gmail.com>
2016-11-17 11:29:21 -0800

    Asyncio loop now closes on keyboard interrupt. Moved inline javascript function to its own .js file. Added type checking to the shared ref constructor.

--- python/dyno_main.py
+++ python/dyno_main.py
@@ -17,37 +17,41 @@ sample_freq_r = shared_ref(Lock(), 1E-5)
 
 async def data_transmission(websocket, path):
 
-	data = "f%d" % sample_freq
-	await websocket.send(data)
+    data = "f%d" % sample_freq_r.get()
+    await websocket.send(data)
 
-	while True:
-		data = "s%.0f" % speed_r.get()
-		await websocket.send(data)
-		data = "T%.1f" % torque_r.get()
-		await websocket.send(data)
-		data = "t%.2f" % time_r.get()
-		await websocket.send(str(data))
-		await asyncio.sleep(1/sample_freq.get())
+    while True:
+        data = "s%.0f" % speed_r.get()
+        await websocket.send(data)
+        data = "T%.1f" % torque_r.get()
+        await websocket.send(data)
+        data = "t%.2f" % time_r.get()
+        await websocket.send(str(data))
+        await asyncio.sleep(1/sample_freq_r.get())
 
 #------------------------------------------------------------------------------------------------------------------
 #   M A I N  
 #------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-	# initialize sample frequency to 50Hz
-	sample_freq_r.put(50)
+    # initialize sample frequency to 50Hz
+    sample_freq_r.put(50)
 
-	# launch hall effect thread
-	hall_eff_thread = hall_effect_thread(115200, 200E-6, sample_freq, speed_r, torque_r, time_r)
-	hall_eff_thread.start()
+    # launch hall effect thread
+    hall_eff_thread = hall_effect_thread(115200, 200E-6, sample_freq_r.get(), speed_r, torque_r, time_r)
+    hall_eff_thread.start()
 
-	# launch websocket and run it forever
-	server = websockets.serve(data_transmission, '127.0.0.1', 8001)
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(server)
-	loop.run_forever()
+    # launch websocket and run it forever
+    loop = asyncio.get_event_loop()
+    server = websockets.serve(data_transmission, '127.0.0.1', 8001)
+    loop.run_until_complete(server)
+    try:
+        loop.run_forever()
+    except:
+        loop.close()
 
 
 
-					
+
+                    
 
--- python/thread_safe.py
+++ python/thread_safe.py
@@ -6,9 +6,17 @@ from threading import Lock
 class shared_ref():
 
 	def __init__(self, my_lock, my_timeout, my_val=None): 
+
 		self._lock = my_lock
 		self._timeout = my_timeout
 		self._value = my_val
+
+		# ensure the input parameter my_lock is of class threading.Lock()
+		try:
+			self._lock.acquire(timeout=self._timeout)
+			self._lock.release()
+		except:
+			raise TypeError('The first input parameter was not of class threading.Lock()')
 
 	def get(self): 
 		try:
